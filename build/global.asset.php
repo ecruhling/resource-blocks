ab2ea7429dbf50526c3a12f2ffa957e46046533d
erik <ecruhling@gmail.com>
2022-05-12 09:48:55 -0500

    resource-disabled class only on list items
    Signed-off-by: ecruhling <erik@resourceatlanta.com>

--- build/global.asset.php
+++ build/global.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-core-data', 'wp-data', 'wp-dom-ready', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => 'ad0943d9e41eb1eef49e2a89d1ec74db');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-core-data', 'wp-data', 'wp-dom-ready', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '2ac019c46f91165c699141644c1bc2e1');
